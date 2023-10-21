ef1266a7d926f0e795cb45d96a93221a2d608686
Svetlana Nasekina <liylic@rambler.ru>
2023-10-21 21:07:31 +0300

    задача05

---
+++ task03/task05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+  
+   int N = - number;  
+while(N < number + 1)
+
+
+{Console.Write(N + " ");
+
+   N = N + 1;
+}
