4e252a1d03b53cd173eeaff7f2fc0a6885e2ebc6
Svetlana Nasekina <liylic@rambler.ru>
2023-10-22 16:23:04 +0300

    задача07

---
+++ task03/task05/task07/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа.*/
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+int result = number%10;
+{ Console.WriteLine(result); }
