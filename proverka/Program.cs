ad339ff61b759ced349dca74718a0ed106c27824
Svetlana Nasekina <liylic@rambler.ru>
2023-11-09 14:28:13 +0300

    добавили задачу 09

---
+++ proverka/Program.cs
@@ -0,0 +1,3 @@
+﻿
+Console.WriteLine("Proverka");
+Console.WriteLine("task");
---
+++ task09/Program.cs
@@ -0,0 +1,10 @@
+﻿// 9. Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает 
+//наибольшую цифру числа.
+int n  = new Random().Next(10,99);
+Console.WriteLine(n);
+int max = 0;
+do max =Math.Max(max,n%10);
+while((n/=10)!=0);
+
+
+Console.WriteLine(max);
