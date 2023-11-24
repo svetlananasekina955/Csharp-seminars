e5cccaf5c283ca09b8097fa953b2b28369510b50
Svetlana Nasekina <liylic@rambler.ru>
2023-11-24 19:20:29 +0300

    добавили задачу 30

---
+++ task30/Program.cs
@@ -0,0 +1,20 @@
+﻿//30. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+ int[] GetRnd(int size)
+{
+int[] array = new int[size];
+
+ for(int i = 0; i < size; i++)
+  {
+  array[i] = new Random().Next(0,2);
+}
+    return array;
+}
+ void PrintArray(int[] arr)
+ {
+for(int i = 0; i < arr.Length; i++)
+{
+System.Console.Write(arr[i] + " ");
+}
+ }
+int[] userArray = GetRnd(8);
+PrintArray(userArray);
